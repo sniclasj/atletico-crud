f1d58329e873fa7d099415115cb06fdf766e0fb1
sniclasj <sniclasj@gmail.com>
2022-05-13 14:55:22 +0000

    Import country, league, club, player from models file

--- atleticocrud/routes.py
+++ atleticocrud/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from atleticocrud import app, db
-from atleticocrud.models import Country, League
+from atleticocrud.models import Country, League, Club, Player
 
 
 @app.route("/")
