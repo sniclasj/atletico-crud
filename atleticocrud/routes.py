cda5fd1bd3ebfcf04bfb6db35eb62df5b25ce17a
sniclasj <sniclasj@gmail.com>
2022-05-15 10:02:50 +0000

    Add add_country route to allow template generation

--- atleticocrud/routes.py
+++ atleticocrud/routes.py
@@ -6,3 +6,8 @@ from atleticocrud.models import Country, League, Club, Player
 @app.route("/")
 def home():
     return render_template("countries.html")
+
+
+@app.route("/add_country", methods = ["GET", "POST"])
+def add_country():
+    return render_template("add_country.html")
