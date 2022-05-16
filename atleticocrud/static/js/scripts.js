d43f02db1b964b8a34b0c603cccc76505d5ee394
sniclasj <sniclasj@gmail.com>
2022-05-16 08:41:54 +0000

    Add modal initialization to scripts

--- atleticocrud/static/js/scripts.js
+++ atleticocrud/static/js/scripts.js
@@ -3,3 +3,9 @@ document.addEventListener('DOMContentLoaded', function() {
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
   });
+
+// Modal initialization
+  document.addEventListener('DOMContentLoaded', function() {
+    let modal = document.querySelectorAll('.modal');
+    M.Modal.init(modal);
+  });
