f88006d0b0b6862f9e9b00bccb967fffe4f006bf
sniclasj <sniclasj@gmail.com>
2022-05-24 08:53:44 +0000

    Import migrate from flask_migrate and define migrate

--- atleticocrud/__init__.py
+++ atleticocrud/__init__.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 if os.path.exists("env.py"):
     import env  # noqa
 
@@ -10,5 +11,6 @@ app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 from atleticocrud import routes  # noqa
