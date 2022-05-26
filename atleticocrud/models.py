8a94b252cce8846b5463af046cc542779d30d319
sniclasj <sniclasj@gmail.com>
2022-05-26 14:28:25 +0000

    Add repr self function to users model

--- atleticocrud/models.py
+++ atleticocrud/models.py
@@ -54,3 +54,7 @@ class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_name = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(260), nullable=False)
+    
+    def __repr__(self):
+        # __repr__ to represent itself in the form of a string
+        return self.user_name
