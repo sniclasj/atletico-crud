e3d8b1a8c102c815658247ce664b3f80401df36c
sniclasj <sniclasj@gmail.com>
2022-05-26 14:26:25 +0000

    Remove verify password from users model

--- atleticocrud/models.py
+++ atleticocrud/models.py
@@ -54,4 +54,3 @@ class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_name = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(260), nullable=False)
-    verify_password = db.Column(db.String(260), nullable=False)
