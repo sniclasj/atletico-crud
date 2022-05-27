5f5d5b5e9dab52b63f5fa13f3236e7d7ef03a143
sniclasj <sniclasj@gmail.com>
2022-05-27 09:29:14 +0000

    Add login code GET method to routes

--- atleticocrud/routes.py
+++ atleticocrud/routes.py
@@ -35,6 +35,11 @@ def register():
     return render_template("register.html")
 
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    return render_template("login.html")
+
+
 @app.route("/countries")
 def countries():
     countries = list(Country.query.order_by(Country.country_name).all())
