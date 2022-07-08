2756544e35750abd8002247da25ee6fc529754cc
sniclasj <sniclasj@gmail.com>
2022-07-08 12:37:06 +0000

    Correct typo in if statement

--- atleticocrud/__init__.py
+++ atleticocrud/__init__.py
@@ -18,9 +18,9 @@ if os.environ.get("DEVELOPMENT") == "True":
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 else:
     uri = os.environ.get("DATABASE_URL")
-    if uri.startwith("postgres://"):
+    if uri.startswith("postgres://"):
         uri.replace("postgres://", "postgresql://", 1)
-    app.config["SQLALCHEMY_DATABASE_URI"] = uri # heroku
+    app.config["SQLALCHEMY_DATABASE_URI"] = uri  # heroku
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
