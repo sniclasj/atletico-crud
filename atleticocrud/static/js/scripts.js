2faf3523234f3e8d9e97d1399e764312d82ff3e4
sniclasj <sniclasj@gmail.com>
2022-07-14 15:48:31 +0000

    Add jshint code line and define datepicker variable

--- atleticocrud/static/js/scripts.js
+++ atleticocrud/static/js/scripts.js
@@ -1,27 +1,29 @@
+/* jshint esversion: 11 */
+
 // Materialize sidenav initialization
 document.addEventListener('DOMContentLoaded', function() {
-    let sidenav = document.querySelectorAll('.sidenav');
-    M.Sidenav.init(sidenav);
-  });
+  let sidenav = document.querySelectorAll('.sidenav');
+  M.Sidenav.init(sidenav);
+});
 
 // Materialize modal initialization
-  document.addEventListener('DOMContentLoaded', function() {
-    let modal = document.querySelectorAll('.modal');
-    M.Modal.init(modal);
-  });
+document.addEventListener('DOMContentLoaded', function() {
+  let modal = document.querySelectorAll('.modal');
+  M.Modal.init(modal);
+});
 
 // Materialize dropdown list initialization
-  document.addEventListener('DOMContentLoaded', function() {
-    let dropdown = document.querySelectorAll('select');
-    M.FormSelect.init(dropdown);
-  });
+document.addEventListener('DOMContentLoaded', function() {
+  let dropdown = document.querySelectorAll('select');
+  M.FormSelect.init(dropdown);
+});
 
 // Materialize datepicker initialization
-  document.addEventListener('DOMContentLoaded', function() {
-    datepicker = document.querySelectorAll('.datepicker');
-    M.Datepicker.init(datepicker, {
-      format: "dd mmm yyyy",
-      yearRange: 100,
-      i18n: {done: "Select"}
-    });
+document.addEventListener('DOMContentLoaded', function() {
+  let datepicker = document.querySelectorAll('.datepicker');
+  M.Datepicker.init(datepicker, {
+    format: "dd mmm yyyy",
+    yearRange: 100,
+    i18n: {done: "Select"}
   });
+});
