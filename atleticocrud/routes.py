0bfbe456701b0d4a80d9c697d5d89fb2029bb44d
sniclasj <sniclasj@gmail.com>
2022-07-19 10:18:54 +0000

    Add error handling routes for 404 and 500

--- atleticocrud/routes.py
+++ atleticocrud/routes.py
@@ -456,3 +456,15 @@ def confirmation():
         return redirect(url_for("profile", username=session["user"]))
     else:
         return render_template("confirmation.html")
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def page_not_found(e):
+    # note that we set the 500 status explicitly
+    return render_template('500.html'), 500
